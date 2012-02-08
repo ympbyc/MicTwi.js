7ffac55aa17bc95ed68c8f2cffa048839c2f938a
Minori Yamashita <ympbyc@gmail.com>
2012-02-09 01:20:24 +0900

    first commit

---
+++ MicTwi/mictwi.js
@@ -0,0 +1,126 @@
+/*
+ * MicTwi is a client-side javascript module that allows your script to 
+ * make ajax requests to twitter's REST APIs.
+ * MicTwi is equiped with following methods and attributes:
+ * MicTwi
+ *   oauthProxy: "http://proxy-url"
+ *   callbackUrl: "http://where.to/come/back/after/oauth"
+ *   generateGetAPI: function (api) { //returns a function to access to the twitter api specified by the argument.
+ *   generatePostAPI: function (api) { //same as above but with POST method. 
+ *   oauth: function (callback) { //redirect users to twitter's OAuth prompt only when they are not authorized.
+ *   ifAuthorized: function (whent, whenf) { //call whent if authorized, otherwise whenf will be called.
+ *   
+ * Example:
+ * MicTwi.oauth(function () {
+ *  var foo = MicTwi.generateGetAPI('users/show.json');
+ *  foo({screen_name: 'ympbyc'},function(obj){
+ *    alert(obj.name + ' : ' + obj.description);
+ *  });
+ * });
+ */
+
+var MicTwi = (function (){
+  var generateGetAPI = function (api) {
+    return (arguments[1] == false) ?
+      function (param, callback){ //oauth NOT required
+        jsonp({
+          url: 'https://api.twitter.com/1/' + api,
+          jsonp: 'callback',
+          data: param || {},
+          success: function (json) { (callback || function(){}) (json); }
+        });
+      }
+    :
+      function (param, callback) { //oauth required
+        jsonp({
+          url: MicTwi.oauthProxy + '/get/twitter',
+          jsonp: "callback",
+          data: {api : api,
+                 param : JSON.stringify((param || {}))},
+          success: function (json) { (callback || function(){}) (json); }
+        });
+      };
+  }
+  
+  var generatePostAPI = function (api) {
+    return function (param, callback) {
+      param = param || {};
+      jsonp({
+        url: MicTwi.oauthProxy + '/post/twitter',
+        jsonp: "callback",
+        data: {api : api,
+               param : JSON.stringify((param || {}))},
+        success: function (json) { (callback || function(){}) (json); }
+      });
+    };
+  }
+  
+  var makeQueryString = function (obj) {
+    var param_str = '?';
+    for (var i in obj) {
+      param_str += i + '=' + obj[i] + '&';
+    }
+    return (param_str.length > 1) ? param_str.slice(0,-1) : '?dummy';
+  }
+
+  var jsonp = function (o) {
+    var callbackName = 'MicTwi' + (new Date).getTime();
+    window[callbackName] = o.success;
+    var s = document.createElement('script');
+    s.src = o.url + makeQueryString(o.data) + '&' + o.jsonp + '=' + callbackName;
+    s.id = callbackName;
+    document.body.appendChild(s);
+    setTimeout(function () {
+      delete window[callbackName];
+      document.body.removeChild(document.getElementById(callbackName))
+    }, 5000);
+  }
+
+  var MicTwi = {
+    oauthProxy: "http://tofuchaproxy-ympbyc.dotcloud.com", // Proxy server that handles OAuth requests.
+    callbackUrl: location.origin + location.pathname, // Page to return after OAuth Authorization.
+        
+    /* call whent if authorized, otherwise whenf will be called */
+    ifAuthorized: function(whent, whenf) {
+      whenf = whenf || function(){};
+      jsonp({
+        url: 'http://tofuchaproxy-ympbyc.dotcloud.com/is/authorized',
+        data: {dummy: 'dummy'}, jsonp: 'callback', 
+        success: function (bool) { bool? whent() : whenf(); }
+      });
+    },
+    
+    oauth: function (callback) {
+      MicTwi.ifAuthorized(
+        callback || function(){},
+        function(){
+          location.href = MicTwi.oauthProxy + '/auth/twitter?callback=' + MicTwi.callbackUrl;
+        });
+    },
+
+    /* Section below exists for convenience and an example.
+     * By binding the API functions to these objects provided here
+     * you can make your code look uniformed ;) */
+    statuses: {
+      update: generatePostAPI('statuses/update.json'),
+      home_timeline: generateGetAPI('statuses/home_timeline.json'),
+      mentions: generateGetAPI('statuses/mentions.json'),
+      /* Generate as much API functions as you want just like this. 
+       * Alternartively you can simply call MicTwi.generateGetAPI('whatever/provided.json') 
+       * from your code and use the resulted function to make requests.
+       * List of APIs twitter provides can be found at https://dev.twitter.com/docs/api */
+    },
+    search: {},       direct_messages: {},  followers: {},        friends: {}, 
+    friendships: {},  users: {},            favorites: {},        lists: {}, 
+    account: {},      notifications: {},    saved_searches: {},   trends: {}, 
+    geo: {},          blocks: {},           report_spam: {},      help: {},     legal: {},
+  }
+  
+  /* For APIs which take variables (often they are user-ids or status-ids), 
+   * you ought to generate an API function on the fly with MicTwi.generateGetAPI() / MicTwi.generatePostAPI.
+   * eg: MicTwi.favorites.create = MicTwi.generatePostAPI('favorites/create/' + 123456 + '.json'); */
+  MicTwi.generateGetAPI = generateGetAPI;
+  MicTwi.generatePostAPI = generatePostAPI; 
+  
+  return MicTwi;
+})();
---
+++ tofucha/server.js
@@ -0,0 +1,129 @@
+var express = require('express'),
+    ejs = require('ejs'),
+    oauth = (new (require('oauth').OAuth)(
+        'https://api.twitter.com/oauth/request_token',
+        'https://api.twitter.com/oauth/access_token',
+        'ConSUmErkeY', //consumer key
+        'coNSUm3R534cr37', //consumer secret
+        '1.0',
+        'http://yourserver/auth/twitter/callback', //callback url
+        'HMAC-SHA1')
+    );
+    
+var makeQueryString = function (obj) {
+  var param_str = '?';
+  for (var i in obj) {
+    param_str += i + '=' + obj[i] + '&';
+  }
+  return param_str.slice(0,-1);
+}
+
+var app = express.createServer();
+app.configure(function() {
+    app.use(express.logger());
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: "tofucha" }));
+    app.use(app.router);
+    app.register('.ejs', ejs);
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');
+    //app.use(express.static(__dirname + '/resources'));
+});
+
+app.dynamicHelpers({
+    session: function(req, res) {
+        return req.session;
+    }
+});
+
+app.get('/', function (req, res) {
+    res.send('Usage: http://tofuchaproxy-ympbyc.dotcloud.com/auth/twitter?callback=http://your.app/foo');
+});
+
+app.get('/auth/twitter', function (req, res) {
+    req.session.callback = req.query.callback;
+    oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+        if(error) {
+            res.send(error)
+        } else {
+            req.session.oauth = {};
+            req.session.oauth.token = oauth_token;
+            req.session.oauth.token_secret = oauth_token_secret;
+            res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token);
+        }
+    });
+});
+
+app.get('/auth/twitter/callback', function(req, res){
+  if(req.session.oauth) {
+        req.session.oauth.verifier = req.query.oauth_verifier;
+        oauth.getOAuthAccessToken(req.session.oauth.token, req.session.oauth.token_secret, req.session.oauth.verifier,
+            function(error, oauth_access_token, oauth_access_token_secret, results) {
+                if(error) {
+                    res.send(error);
+                } else {
+                  req.session.oauth_access_token = oauth_access_token;
+                  req.session.oauth_access_token_secret = oauth_access_token_secret;
+                  res.redirect(req.session.callback + '?authorized=yes');
+                }
+            });
+   }
+});
+
+app.get('/post/twitter', function (req, res) {
+    console.log(req.query.param);
+    if (!req.session.oauth_access_token){res.send(401); return;} //unauthorized
+    try {
+      if (!req.query.api) throw 'no api specified';
+      var jsonParam = JSON.parse(req.query.param);
+    } catch (e) {
+      res.send(400); return;
+    }
+    
+    oauth.post("https://api.twitter.com/1/" + req.query.api,
+               req.session.oauth_access_token, 
+               req.session.oauth_access_token_secret, 
+               jsonParam, //{"status":req.body.status},
+               function (error, data) {
+                    if (error) {
+                        res.send(502);
+                    }
+                    else {
+                        res.send( req.query.callback + '(' + data + ');', {'content-type' : 'text/javascript'}, 200);
+                    }
+               });
+});
+
+app.get('/get/twitter', function (req, res) {
+    console.log("https://api.twitter.com/1/" + req.query.api + req.query.param);
+    if (!req.session.oauth_access_token){res.send(401); return;} //unauthorized
+    try {
+      if (!req.query.api) throw 'no api specified';
+      var jsonParam = JSON.parse(req.query.param);
+    } catch (e) {
+      console.log(e);
+      res.send(400); return;
+    }
+    
+    oauth.get("http://api.twitter.com/1/" + req.query.api + makeQueryString(jsonParam),
+               req.session.oauth_access_token, 
+               req.session.oauth_access_token_secret, 
+               function (error, data) { 
+                    if (error) {
+                        res.send(502);
+                    }
+                    else {
+                        res.send( req.query.callback + '(' + data + ');', {'content-type' : 'text/javascript'}, 200);
+                    }
+               });
+});
+
+app.get('/is/authorized', function (req, res) {
+  if (!!req.session.oauth_access_token && !!req.session.oauth_access_token_secret)
+    res.send(req.query.callback + '(true);', {'content-type' : 'text/javascript'}, 200);
+  else
+    res.send(req.query.callback + '(false);', {'content-type' : 'text/javascript'}, 200);
+});
+
+app.listen(8080);
